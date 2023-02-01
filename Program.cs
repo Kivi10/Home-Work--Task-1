3ed53ca710da704d60652522d920538f39023dca
Hamzat <hgeroev11@mail.com>
2023-02-01 22:52:36 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+System.Console.Write("Past the number A -> ");
+int number_A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Past the number B -> ");
+int number_B = Convert.ToInt32(Console.ReadLine());
+
+if(number_A > number_B)
+{
+    System.Console.WriteLine($"max = {number_A}");
+}
+else System.Console.WriteLine($"max = {number_B}");
